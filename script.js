0d1ea0b6b73c4c4ba643837ef0eebe4458880b35
ThibautWeb <216151628+ThibautWeb@users.noreply.github.com>
2025-08-04 12:46:24 +0200

    Add functional screen and input rules for digits and point
    Display operand on screen while typing, as expected

--- script.js
+++ script.js
@@ -1,14 +1,46 @@
-document.querySelectorAll(".digit").forEach(el => {
-  el.addEventListener("click", e => inputDigit(e.target.value))
-})
+let rightOperand = "0"
+let leftOperand = ""
+let operator =""
 
+document.querySelectorAll(".digit")
+  .forEach(el => el.addEventListener("click", e => inputDigit(e.target.value)))
 function inputDigit(val) {
-  console.log(val)
+  if (rightOperand === "" || rightOperand === "0") {
+    rightOperand = val
+  } else {
+    rightOperand += val
+  }
+  let screen = document.querySelector(".screen p")
+  screen.textContent = rightOperand
 }
 
-document.querySelector("button[value='.']").addEventListener("click", () => console.log("comma"))
-document.querySelector("button[value='+']").addEventListener("click", () => console.log("add"))
-document.querySelector("button[value='-']").addEventListener("click", () => console.log("substract"))
-document.querySelector("button[value='*']").addEventListener("click", () => console.log("multiply"))
-document.querySelector("button[value='/']").addEventListener("click", () => console.log("divide"))
-document.querySelector("button[value='=']").addEventListener("click", () => console.log("equal"))
+document.querySelector("button[value='.']").addEventListener("click", inputPoint)
+function inputPoint() {
+  if (rightOperand !== "" && !rightOperand.includes(".")) {
+    rightOperand += "."
+    let screen = document.querySelector(".screen p")
+    screen.textContent = rightOperand
+  }
+}
+
+document.querySelector("button[value='+']").addEventListener("click", inputPlus)
+function inputPlus() {
+  console.log("plus")
+}
+document.querySelector("button[value='-']").addEventListener("click", inputMinus)
+function inputMinus() {
+  console.log("minus")
+}
+document.querySelector("button[value='*']").addEventListener("click", inputMultiply)
+function inputMultiply() {
+  console.log("multiply")
+}
+document.querySelector("button[value='/']").addEventListener("click", inputDivide)
+function inputDivide() {
+  console.log("divide")
+}
+document.querySelector("button[value='=']").addEventListener("click", inputEqual)
+function inputEqual() {
+  console.log("equal")
+}
+
