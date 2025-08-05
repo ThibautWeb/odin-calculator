fe1bf94c57e8f05e79c0d0483a88f748339297e8
ThibautWeb <216151628+ThibautWeb@users.noreply.github.com>
2025-08-05 19:22:05 +0200

    Add addition and equal functionalities
    Addition and equal buttons now work as expected

--- script.js
+++ script.js
@@ -6,13 +6,14 @@ let screen = document.querySelector(".screen p")
 // DIGITS AND POINT
 function pressDigit(val) {
   if (newNumber === "0") {
+    if (!operator) savedNumber = "0"
     if (val !== "0") {
       newNumber = val
     }
   } else {
      newNumber += val
   }
-  updateScreen()
+  updateScreen(newNumber)
 }
 document.querySelectorAll(".digit").forEach(el => el.addEventListener("click", e => pressDigit(e.target.value)))
 
@@ -20,13 +21,15 @@ function pressPoint() {
   if (!newNumber.includes(".")) {
     console.log(newNumber)
     newNumber += "."
-    updateScreen()
+    updateScreen(newNumber)
   }
 }
 document.querySelector("button[value='.']").addEventListener("click", pressPoint)
 
 // OPERATIONS
 function pressPlus() {
+  operator = "+"
+  operate()
 }
 document.querySelector("button[value='+']").addEventListener("click", pressPlus)
 
@@ -43,10 +46,18 @@ function pressDivide() {
 document.querySelector("button[value='/']").addEventListener("click", pressDivide)
 
 function pressEqual() {
+  if (!operator) return
+  operate()
+  operator = ""
 }
 document.querySelector("button[value='=']").addEventListener("click", pressEqual)
 
 // UTILITIES
-function updateScreen() {
-  screen.textContent = newNumber
+function operate() {
+  if (operator === "+") savedNumber = +savedNumber + +newNumber
+  newNumber = "0"
+  updateScreen(savedNumber)
+}
+function updateScreen(number) {
+  screen.textContent = number
 }
