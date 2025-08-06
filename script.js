9922fe4d34e8031b1f049f3c175a9ce908bf2c2d
ThibautWeb <216151628+ThibautWeb@users.noreply.github.com>
2025-08-06 08:18:59 +0200

    Reset script
    Trying to get it right with clean, patient pseudo-code this time

--- script.js
+++ script.js
@@ -5,31 +5,15 @@ let screen = document.querySelector(".screen p")
 
 // DIGITS AND POINT
 function pressDigit(val) {
-  if (newNumber === "0") {
-    if (!operator) savedNumber = "0"
-    if (val !== "0") {
-      newNumber = val
-    }
-  } else {
-     newNumber += val
-  }
-  updateScreen(newNumber)
 }
 document.querySelectorAll(".digit").forEach(el => el.addEventListener("click", e => pressDigit(e.target.value)))
 
 function pressPoint() {
-  if (!newNumber.includes(".")) {
-    console.log(newNumber)
-    newNumber += "."
-    updateScreen(newNumber)
-  }
 }
 document.querySelector("button[value='.']").addEventListener("click", pressPoint)
 
 // OPERATIONS
 function pressPlus() {
-  operator = "+"
-  operate()
 }
 document.querySelector("button[value='+']").addEventListener("click", pressPlus)
 
@@ -46,17 +30,16 @@ function pressDivide() {
 document.querySelector("button[value='/']").addEventListener("click", pressDivide)
 
 function pressEqual() {
-  if (!operator) return
-  operate()
-  operator = ""
 }
 document.querySelector("button[value='=']").addEventListener("click", pressEqual)
 
 // UTILITIES
 function operate() {
-  if (operator === "+") savedNumber = +savedNumber + +newNumber
-  newNumber = "0"
-  updateScreen(savedNumber)
+}
+
+function saveNumber() {
+  savedNumber = newNumber
+  newNumber = ""
 }
 function updateScreen(number) {
   screen.textContent = number
